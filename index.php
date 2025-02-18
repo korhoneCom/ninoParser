0cf4d8b178bcbd3b8e71d21166997d3e65bdbe33
korhoneCom <103757909+korhoneCom@users.noreply.github.com>
2025-02-18 12:53:10 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,11 +8,6 @@ for($i=1;$i<count($nino);$i++)
     $pageUrl = $ninoUrl[2];
     $ninoPage = file_get_contents($pageUrl);
     $page = explode("post-body",$ninoPage);
-for($j=1;$j<count($page);$j++)
-  {
-    print("$j <br>");
-    print($page[$j]);
-    print("<br><br>");
-  }
+    print($page[48]);
   }
 ?>
