caab922820d79f18bd2f834467ae9f7266a4a900
korhoneCom <103757909+korhoneCom@users.noreply.github.com>
2025-02-18 12:41:44 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 $url = "https://www.ninoiptv.com";
 $ninoiptv = file_get_contents($url);
 $nino = explode("entry-title-link",$ninoiptv);
-print(count($nino));
 for($i=1;$i<count($nino);$i++)
   {
     $ninoUrl = explode("'",$nino[$i]);
