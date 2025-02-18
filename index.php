ca5ecd18925c131888bbaeaf1203d5908650c115
korhoneCom <103757909+korhoneCom@users.noreply.github.com>
2025-02-18 11:45:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,4 +3,8 @@ $url = "https://www.ninoiptv.com";
 $ninoiptv = file_get_contents($url);
 $nino = explode("entry-title-link",$ninoiptv);
 print($nino[1]);
+for($i=1;$i<count($nino);$i++)
+  {
+    $ninoUrl = explode("\",$nino[$i]);
+  }
 ?>
